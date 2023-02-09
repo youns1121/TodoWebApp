808b8716a689646ea6d567f2745757d6880f8f6e
youns <minys94@daum.net>
2023-02-10 00:59:21 +0900

    create 메서드

--- src/main/java/com/todowebapp/persistence/TodoRepository.java
+++ src/main/java/com/todowebapp/persistence/TodoRepository.java
@@ -3,5 +3,9 @@ package com.todowebapp.persistence;
 import com.todowebapp.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+
+    List<TodoEntity> findByUserId(String userId);
 }
--- src/main/java/com/todowebapp/service/TodoService.java
+++ src/main/java/com/todowebapp/service/TodoService.java
@@ -1,13 +1,40 @@
 package com.todowebapp.service;
 
+import com.todowebapp.model.TodoEntity;
+import com.todowebapp.persistence.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class TodoService {
 
+    private final TodoRepository todoRepository;
+
     public String testService(){
         return "Test Service";
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+
+        if(entity == null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity be null");
+        }
+
+        if (entity.getUserId() == null){
+            log.warn("Unknown user");
+            throw new RuntimeException("Unknown user");
+        }
+
+        todoRepository.save(entity);
+
+        log.info("Entity Id : {} is saved", entity.getId());
+
+        return todoRepository.findByUserId(entity.getUserId());
     }
 }
