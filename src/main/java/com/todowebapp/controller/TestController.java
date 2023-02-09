f2695b4179cf16a00f571ea8bee41fa7147abd71
youns <minys94@daum.net>
2023-02-10 00:30:52 +0900

    초기 설정

---
+++ src/main/java/com/todowebapp/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.todowebapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello world!";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath() {
+        return "Hello World! testGetMapping";
+    }
+
+}
